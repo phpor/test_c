bb4032a473d389ccf6ae79a4ce3dd74bf0aece06
phpor0 <phpor0@sina.com>
2015-12-07 17:12:38 +0800

    modify malloc.c

--- malloc.c
+++ malloc.c
@@ -7,14 +7,16 @@ int main(int ac, char** av) {
 	char *p;
 	char s[16] = "";
 	int i = 0, sum = 0;
-	while(true) {
+	while(1) {
+		printf("please input num (MB) to malloc or 'q' to exit: ");
 		if (gets(s) == NULL) continue;
+		if (strcmp("q", s) == 0) break;
 		if (0 == strlen(s)) continue;
 		int cnt = atoi(s);
 		if (cnt == 0 ) continue;
 
 		p = malloc(1024*1024* cnt);
-		memset(p, cnt, 1024*1024);
+		memset(p, '\0', 1024*1024* cnt );
 		sum += cnt;
 		printf("malloced %d\n", sum);
 	}
