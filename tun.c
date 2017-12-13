018f4ab46edeae5e99b5d1ba28fa59c2240d2437
李俊杰 <phpor@users.noreply.github.com>
2017-12-13 17:18:23 +0800

    Update tun.c
    循环等待实现

--- tun.c
+++ tun.c
@@ -12,7 +12,6 @@
 #include <errno.h>
 #include <unistd.h>
 
-#include <sys/select.h>
 
 int tun_alloc(int flags, char *devname)
 {
@@ -69,27 +68,14 @@ int main(int ac, char **av)
 		perror("Allocating interface");
 		exit(1);
 	}
-	FD_ZERO(&set); /* clear the set */
 	fcntl(fd, F_SETFL, O_NONBLOCK);
-	while (1) {
-		FD_SET(fd, &set); /* add our file descriptor to the set */
-		timeout.tv_sec = 1;
-		timeout.tv_usec = 0;
-		ret = select(fd + 1, &set, NULL, NULL, &timeout);
-		if (ret == -1 ) {
-			perror("Reading from interface");
-			break;
-		}
-		if (ret == 0 ) continue;
-		while(nread = read(fd, buffer, sizeof(buffer)) > 0) {
-			write(1, buffer, nread);
-		}
-		if (nread < 0) {
-			perror(strerror(errno));
-			continue;
-		}
-		//printf("Read %d bytes from tun/tap device\n", nread);
+	while(1) {
+			nread = read(fd, buffer, sizeof(buffer));
+			if (nread > 0) write(1, buffer, nread);
+			if (nread < 0 && errno == EAGAIN || nread > 0 && nread < sizeof(buffer)) sleep(1);
+			if (nread < 0 && errno != EAGAIN ) break;
 	}
 	close(fd);
 	return 0;
 }
+//vim: se ts=4
